4cefd12d277b894f10f7b67cb3eb4ad244d58c99
kosta.fortumanov <kosta.fortumanov@sorsix.com>
2024-12-04 09:37:29 +0100

    day 4

--- src/main/kotlin/Util.kt
+++ src/main/kotlin/Util.kt
@@ -2,6 +2,12 @@ import java.io.File
 
 fun readFileLines(day: Int): List<String> = File({}.javaClass.getResource("input_$day.txt")!!.path).readLines()
 
+private fun readFileMatrix(day: Int): List<List<String>> =
+    readFileLines(day).map { line -> line.split("").filter { it.isNotEmpty() } }
+
+fun readFileCharMatrix(day: Int): List<List<Char>> =
+    readFileMatrix(day).map { line -> line.map { it.first() } }
+
 fun String.integers() = Regex("-?\\d+").findAll(this).map { it.value.toInt() }.toList()
 
 fun <T> List<List<T>>.transpose(): List<List<T>> = List(first().size) { i -> List(size) { j -> this[j][i] } }
---
+++ src/main/kotlin/day04/Day04.kt
@@ -0,0 +1,49 @@
+package day04
+
+import println
+import readFileCharMatrix
+
+fun main() {
+    val input = readFileCharMatrix(4)
+
+    part1(input).println()
+    part2(input).println()
+}
+
+private fun part1(input: List<List<Char>>): Int {
+    val directions = listOf(1 to 0, 1 to 1, 1 to -1, 0 to 0, 0 to 1, 0 to -1, -1 to 0, -1 to 1, -1 to -1)
+    return input.sumIndexed { rowIndex, colIndex ->
+        directions.count { direction -> findWord("XMAS", input, rowIndex, colIndex, direction) }
+    }
+}
+
+private fun part2(input: List<List<Char>>): Int = input.sumIndexed { rowIndex, colIndex ->
+    val validFirstDiagonal = findWord("MAS", input, rowIndex, colIndex, 1 to 1) ||
+            findWord("SAM", input, rowIndex, colIndex, 1 to 1)
+    val validSecondDiagonal = findWord("MAS", input, rowIndex, colIndex + 2, 1 to -1) ||
+            findWord("SAM", input, rowIndex, colIndex + 2, 1 to -1)
+
+    if (validFirstDiagonal && validSecondDiagonal) {
+        1
+    } else {
+        0
+    }
+}
+
+private fun List<List<Char>>.sumIndexed(operation: (rowIndex: Int, colIndex: Int) -> Int): Int =
+    foldIndexed(0) { rowIndex, accRow, row ->
+        accRow + row.foldIndexed(0) { colIndex, acc, value ->
+            acc + operation(rowIndex, colIndex)
+        }
+    }
+
+private fun findWord(word: String, input: List<List<Char>>, row: Int, col: Int, direction: Pair<Int, Int>): Boolean {
+    val (dx, dy) = direction
+    val isValid = row in input.indices && col in input[row].indices && row + dx * (word.length - 1) in input.indices
+            && col + dy * (word.length - 1) in input[row].indices
+    return if (isValid) {
+        (0..(word.length - 1)).map { input[row + (dx * it)][col + (dy * it)] }.joinToString("") == word
+    } else {
+        false
+    }
+}
